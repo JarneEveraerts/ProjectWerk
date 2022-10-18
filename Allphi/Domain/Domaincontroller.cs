d71cd09fbec0eea8a5fa8e1766c6a07bd0d1813e
Jarne Everaerts <jarne.everaerts@student.hogent.be>
2022-10-19 01:14:21 +0200

    setup datagridview / selected value to textboxes / add feature coded

--- Allphi/Domain/Domaincontroller.cs
+++ Allphi/Domain/Domaincontroller.cs
@@ -1,4 +1,5 @@
-﻿using Ardalis.GuardClauses;
+﻿using System.Collections;
+using Ardalis.GuardClauses;
 using Domain.Models;
 using Domain.Services;
 using System.Text.RegularExpressions;
@@ -73,17 +74,127 @@ namespace Domain
             return (_parkingRepo.GetAvailableParkingSpotUnreserved());
         }
 
-        #endregion Parking
-
-        public void SubmitVisitor(string licensePlate)
+        public void SubmitVisitorParking(string licensePlate)
         {
             string license = Guard.Against.NullOrEmpty(licensePlate, nameof(licensePlate));
-            ParkingSpot parkingSpot = GetAvailableParkingSpotVisitor();
+            ParkingSpot parkingSpot = _parkingRepo.GetAvailableParkingSpotUnreserved();
             if (IsLicensePlateValid(license) && parkingSpot != null && _parkingRepo.CountParkingSpotByPlate(license) == 0)
             {
                 parkingSpot.Plate = license;
                 _parkingRepo.UpdateParkingSpot(parkingSpot);
             };
+        }
+
+        public void SubmitEmployeeParking(string licensePlate)
+        {
+            string license = Guard.Against.NullOrEmpty(licensePlate, nameof(licensePlate));
+            Employee employee = _employeeRepo.GetEmployeeByPlate(licensePlate);
+            ParkingSpot parkingSpot = _parkingRepo.GetAvailableParkingSpotReserved(employee.Business);
+            if (parkingSpot == null) parkingSpot = _parkingRepo.GetAvailableParkingSpotUnreserved();
+            if (IsLicensePlateValid(licensePlate) && parkingSpot != null &&
+                _parkingRepo.CountParkingSpotByPlate(license) == 0)
+            {
+                parkingSpot.Plate = license;
+                _parkingRepo.UpdateParkingSpot(parkingSpot);
+            }
+        }
+
+        #endregion Parking
+
+        #region Business
+
+        public void CreateBusiness(string name, string? address, string? phone, string email, string btw)
+        {
+            _businessRepo.CreateBusiness(new Business(name, btw, email, address, phone));
+        }
+
+        public List<Business> GetBusinesses()
+        {
+            return _businessRepo.GetBusinesses();
+        }
+
+        #endregion Business
+
+        public List<Visitor> GetVisitors()
+        {
+            return _visitorRepo.GetVisitors();
+        }
+
+        public List<Contract> GetContracts()
+        {
+            return _contractRepo.GetContracts();
+        }
+
+        public List<Employee> GetEmployees()
+
+        {
+            return _employeeRepo.GetEmployees();
+        }
+
+        public List<string> ConvertBusinessToStringList(object input)
+        {
+            Business selected = (Business)input;
+            List<string> business = new List<string>();
+            business.Add(selected.Name);
+            business.Add(selected.Address);
+            business.Add(selected.Phone);
+            business.Add(selected.Email);
+            business.Add(selected.Btw);
+            return business;
+        }
+
+        public List<string> ConvertVisitorToStringList(object input)
+        {
+            Visitor selected = (Visitor)input;
+            List<string> visitor = new List<string>();
+            visitor.Add(selected.Name);
+            visitor.Add(selected.Email);
+            visitor.Add(selected.Plate);
+            visitor.Add(selected.Business);
+            return visitor;
+        }
+
+        public void CreateVisitor(string name, string email, string? plate, string business)
+        {
+            _visitorRepo.CreateVisitor(new Visitor(name, email, business, plate));
+        }
+
+        public List<string> ConvertContractToStringList(object selectedItem)
+        {
+            Contract selected = (Contract)selectedItem;
+            List<string> contract = new List<string>();
+            contract.Add(selected.Business.Name);
+            contract.Add(selected.TotalSpaces.ToString());
+            contract.Add(selected.StartDate.ToString());
+            contract.Add(selected.EndDate.ToString());
+            return contract;
+        }
+
+        public void CreateContract(string spots, string business, DateTime start, DateTime end)
+        {
+            int totalSpots = int.Parse(spots);
+            Business selectedBusiness = _businessRepo.GetBusinessByName(business);
+            _contractRepo.CreateContract(new Contract(selectedBusiness, start, end, totalSpots));
+        }
+
+        public List<string> ConvertEmployeeToStringList(object selectedItem)
+        {
+            Employee selected = (Employee)selectedItem;
+            List<string>? employee = new List<string>();
+            employee.Add(selected.Name);
+            employee.Add(selected.Email);
+            employee.Add(selected.Business.Name);
+            employee.Add(selected.Function);
+            employee.Add(selected.Plate);
+
+            return employee;
+        }
+
+        public void CreateEmployee(string name, string? email, string function, string business, string? plate)
+        {
+            Business selectedBusiness = _businessRepo.GetBusinessByName(business);
+            List<string> names = name.Split(' ').ToList();
+            _employeeRepo.CreateEmployee(new Employee(names[0], names[1], function, selectedBusiness, email, plate));
         }
     }
 }
