9398d6746e2a9d5c946a0c96526cba6345462e54
RobinDeCroock <robin.decroock@student.hogent.be>
2022-12-23 14:25:59 +0100

    ParkingViewModelApp

--- Allphi/Presentation/ViewModels/ParkingAppViewModel.cs
+++ Allphi/Presentation/ViewModels/ParkingAppViewModel.cs
@@ -1,8 +1,15 @@
 ﻿using JetBrains.Annotations;
 using MVVM;
+using Newtonsoft.Json;
+using Shared.Dto;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -13,13 +20,18 @@ namespace Presentation.ViewModels
     public class ParkingAppViewModel : ViewModelBase
     {
         #region private properties
-
+        
         private string _plate;
         private string _business;
+        private RelayCommand _submitCommand;
+        private bool _canSubmit;
+        private ObservableCollection<string> _businessesViews;
+        private HttpClient _api;
         #endregion
 
 
         #region public properties
+        [Required(ErrorMessage ="Nummerplaat mag niet leeg zijn")]
         public string Plate
         {
             get
@@ -32,6 +44,7 @@ namespace Presentation.ViewModels
                 RaisePropertyChanged(); 
             }
         }
+        [Required(ErrorMessage = "Bedrijf mag niet leeg zijn")]
         public string Business
         {
             get
@@ -44,50 +57,77 @@ namespace Presentation.ViewModels
                 RaisePropertyChanged();
             }
         }
-        
 
-        #endregion
-        public ParkingAppViewModel()
-        {
-        }
-
-        private RelayCommand _fetchCommand;
-        public ICommand FetchCommand
+        public ObservableCollection<string> BusinessViews
         {
             get
             {
-                _fetchCommand ??= new RelayCommand(Fetch, param => CanFetch);
-                return _fetchCommand;
+                return _businessesViews;
             }
-        }
-        private bool _canFetch = true;
-
-        public bool CanFetch
-        {
-            get => _canFetch;
-
             set
             {
-                if (_canFetch == value) return;
-                _canFetch = value;
-
+                _businessesViews = value;
                 RaisePropertyChanged();
             }
         }
-        private void Validate(object paramater)
+
+
+        public ICommand SubmitCommand
         {
-            //validate plate and business
-            
-            
-            
+            get
+            {
+                //check if the fields has errors else cansubmit is true
+                _submitCommand ??= new RelayCommand(Submit, param => CanSubmit());
+                return _submitCommand;
+            }
+        }
+
+
+        private bool CanSubmit()
+        {
+            if (string.IsNullOrEmpty(Plate) || string.IsNullOrEmpty(Business))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        #endregion
+        public ParkingAppViewModel(IHttpClientFactory clientFactory)
+        {
+            _businessesViews = new();
+            _api = clientFactory.CreateClient();
+            _api.BaseAddress = new Uri("http://localhost:5038/");
+            FetchBusinesses();
+        }
+
+        private void FetchBusinesses()
+        {
+            //fetch businesses
+            var response = _api.GetAsync("/businesses").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            var businesses = JsonConvert.DeserializeObject<List<BusinessDto>>(content);
+
+            foreach (var item in businesses)
+            {
+                BusinessViews.Add(item.Name);
+            }
 
         }
 
-        private void Fetch(object parameter)
+        private void Submit(object parameter)
         {
-            //fetch data from api/domaincontroller
-            
+            if (!HasErrors)
+            {
+
+            }
         }
 
     }
+
+
 }
+
